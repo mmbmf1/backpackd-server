62b93432aab1d1dec6127581a89642c5726421e4
Michael <mmbmf.mm@gmail.com>
2020-02-05 17:51:05 -0600

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
-const app = require("./app");
-const { PORT } = require("./config");
+const express = require("express");
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
 });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
+
+// const app = require("./app");
+// const { PORT } = require("./config");
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening at http://localhost:${PORT}`);
+// });
