1bd234fdb2c40af30e5bc2a3e70c1b0828484ba0
Michael <mmbmf.mm@gmail.com>
2020-02-09 18:44:23 -0600

    initial commit

---
+++ test/auth-endpoints.spec.js
@@ -0,0 +1,10 @@
+const knex = require('knex')
+const jwt = require('jsonwebtoken')
+const app = require('../src/app')
+const helpers = require('./test-helpers')
+
+// describe('Auth Endpoints', function() {
+//     let db;
+
+//     const { testUsers } = 
+// })
