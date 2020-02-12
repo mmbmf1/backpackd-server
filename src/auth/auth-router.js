9cca269421a85ee65aef1bcb732ae4bc58e2687a
Michael <mmbmf.mm@gmail.com>
2020-02-12 17:03:22 -0600

    minor changes

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -36,6 +36,7 @@ authRouter
                         const payload = { user_id: dbUser.id }
                         res.send({
                             authToken: AuthService.createJwt(sub, payload),
+                            payload
                         })
                     })
             })
--- src/backpacks/backpacks-router.js
+++ src/backpacks/backpacks-router.js
@@ -6,13 +6,17 @@ const { requireAuth } = require("../middleware/jwt-auth");
 const backpacksRouter = express.Router();
 const jsonBodyParser = express.json();
 
-backpacksRouter.route("/").get((req, res, next) => {
+backpacksRouter.route("/").get(requireAuth, (req, res, next) => {
   BackpacksService.getAllBackpacks(req.app.get("db"))
     .then(backpacks => {
       res.json(backpacks);
     })
     .catch(next);
 });
+
+// backpacksRouter
+//   .route('/user_id')
+//   .get((req, res, next) => )
 
 backpacksRouter
   .route("/")
