567ab18d4e7d3e45e930f70cb6f3e1c7b575d103
Michael <mmbmf.mm@gmail.com>
2020-02-11 19:28:44 -0600

    backpack will add with bugs

--- src/backpacks/backpacks-router.js
+++ src/backpacks/backpacks-router.js
@@ -1,20 +1,43 @@
-const express = require('express')
-const BackpacksService = require('./backpacks-service')
-const { requireAuth } = require('../middleware/jwt-auth')
+const express = require("express");
+const path = require("path");
+const BackpacksService = require("./backpacks-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
-const backpacksRouter = express.Router()
-const jsonBodyParser = express.json()
+const backpacksRouter = express.Router();
+const jsonBodyParser = express.json();
+
+backpacksRouter.route("/").get(requireAuth, (req, res, next) => {
+  BackpacksService.getAllBackpacks(req.app.get("db"))
+    .then(backpacks => {
+      res.json(backpacks);
+    })
+    .catch(next);
+});
 
 backpacksRouter
-    .route('/')
-    .get(requireAuth, (req, res, next) => {
-        BackpacksService.getAllBackpacks(req.app.get('db'))
-            .then(backpacks => {
-                res.json(backpacks)
-            })
-            .catch(next)
-    })
+  .route("/")
+  .post(requireAuth, jsonBodyParser, (req, res, next) => {
+    const { name, useritems, total } = req.body;
+    const newBackpack = { name, useritems, total };
 
+    for (const [key, value] of Object.entries(newBackpack))
+      if (value === null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
 
+    console.log(req.user);
 
-module.exports = backpacksRouter    
+    newBackpack.user_id = req.user.id;
+
+    BackpacksService.insertBackpack(req.app.get("db"), newBackpack)
+      .then(backpack => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `${backpack.id}`))
+          .json(BackpacksService.serializeBackpack(backpack));
+      })
+      .catch(next);
+  });
+
+module.exports = backpacksRouter;
--- src/backpacks/backpacks-service.js
+++ src/backpacks/backpacks-service.js
@@ -3,20 +3,43 @@ const xss = require("xss");
 
 const BackpacksService = {
   getAllBackpacks(db) {
-    return db
-      .from("backpackd_backpacks AS bp")
-      .select(
-        "bp.id",
-        "bp.name",
-        "bp.date_created",
-        "bp.useritems",
-        "bp.total"
-        // ...userFields,
-      )
+    return db.from("backpackd_backpacks AS bp").select(
+      "bp.id",
+      "bp.name",
+      "bp.date_created",
+      "bp.useritems",
+      "bp.total"
+      // ...userFields,
+    );
     //   .leftJoin("backpackd_users", "bp.user_id", "user.id");
   },
 
+  insertBackpack(db, newBackpack) {
+    return db
+      .insert(newBackpack)
+      .into("backpackd_backpacks")
+      .returning("*")
+      .then(([backpack]) => backpack)
+      .then(backpack => BackpacksService.getAllBackpacks(db, backpack.id));
+  },
 
+  serializeBackpack(backpack) {
+    const { user } = backpack;
+    return {
+      id: backpack.id,
+      name: xss(backpack.name),
+      useritems: xss(backpack.useritems),
+      total: backpack.total,
+      date_created: new Date(backpack.date_created),
+      // user: {
+      //   id: user.id,
+      //   user_name: user.user_name,
+      //   first_name: user.first_name,
+      //   last_name: user.last_name,
+      //   user_email: user.user_email
+      // },
+    };
+  }
 };
 
 module.exports = BackpacksService;
