d0ac4c959134cac241af8ef6688e1556e5563a6e
Michael <mmbmf.mm@gmail.com>
2020-02-13 10:34:21 -0600

    changing DB_URL

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
-  connectionString: process.env.DB_URL
+  connectionString: process.env.DATABASE_URL
 };
