c97f217541bde2c07043472c001584bbc6d5294e
Michael <mmbmf.mm@gmail.com>
2020-02-13 13:09:08 -0600

    minor changes

--- src/app.js
+++ src/app.js
@@ -3,23 +3,26 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-// const { NODE_ENV } = require("./config");
+const { NODE_ENV } = require("./config");
 const authRouter = require("./auth/auth-router");
 const backpacksRouter = require("./backpacks/backpacks-router");
 const usersRouter = require("./users/users-router");
 
 const app = express();
 
-const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-
 app.use("/api/backpacks", backpacksRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
